a62b6380754b22435944b0b24725c31054c8ba7a
Lawrence Thorpe <techssh@gmail.com>
2015-02-26 16:52:59 +0000

    1

---
+++ Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace cube
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timerText = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.inspectTimerText = new System.Windows.Forms.Label();
+            this.inspectTimer = new System.Windows.Forms.Timer(this.components);
+            this.scrambleText = new System.Windows.Forms.Label();
+            this.delTimeButton = new System.Windows.Forms.Button();
+            this.savedTimes = new System.Windows.Forms.ListBox();
+            this.delAllTimesButton = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.highestTimeLabel = new System.Windows.Forms.Label();
+            this.highestTimeText = new System.Windows.Forms.Label();
+            this.lowestTimeText = new System.Windows.Forms.Label();
+            this.averageTimeText = new System.Windows.Forms.Label();
+            this.lowestTimeLabel = new System.Windows.Forms.Label();
+            this.averageTimeLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timerText
+            // 
+            this.timerText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.timerText.AutoSize = true;
+            this.timerText.Font = new System.Drawing.Font("OCR A Extended", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timerText.ForeColor = System.Drawing.Color.Red;
+            this.timerText.Location = new System.Drawing.Point(12, 9);
+            this.timerText.Name = "timerText";
+            this.timerText.Size = new System.Drawing.Size(340, 66);
+            this.timerText.TabIndex = 0;
+            this.timerText.Text = "00:00:00";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // inspectTimerText
+            // 
+            this.inspectTimerText.AutoSize = true;
+            this.inspectTimerText.Font = new System.Drawing.Font("OCR A Extended", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inspectTimerText.ForeColor = System.Drawing.Color.Yellow;
+            this.inspectTimerText.Location = new System.Drawing.Point(358, 47);
+            this.inspectTimerText.Name = "inspectTimerText";
+            this.inspectTimerText.Size = new System.Drawing.Size(97, 20);
+            this.inspectTimerText.TabIndex = 1;
+            this.inspectTimerText.Text = "00:00:00";
+            this.inspectTimerText.Visible = false;
+            // 
+            // inspectTimer
+            // 
+            this.inspectTimer.Interval = 50;
+            this.inspectTimer.Tick += new System.EventHandler(this.inspectTimer_Tick);
+            // 
+            // scrambleText
+            // 
+            this.scrambleText.AutoSize = true;
+            this.scrambleText.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scrambleText.ForeColor = System.Drawing.Color.Cyan;
+            this.scrambleText.Location = new System.Drawing.Point(21, 75);
+            this.scrambleText.Name = "scrambleText";
+            this.scrambleText.Size = new System.Drawing.Size(20, 22);
+            this.scrambleText.TabIndex = 2;
+            this.scrambleText.Text = "-";
+            // 
+            // delTimeButton
+            // 
+            this.delTimeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.delTimeButton.Location = new System.Drawing.Point(104, 3);
+            this.delTimeButton.Name = "delTimeButton";
+            this.delTimeButton.Size = new System.Drawing.Size(25, 25);
+            this.delTimeButton.TabIndex = 4;
+            this.delTimeButton.TabStop = false;
+            this.delTimeButton.Text = "-";
+            this.delTimeButton.UseVisualStyleBackColor = true;
+            this.delTimeButton.Click += new System.EventHandler(this.delTimeButton_Click);
+            // 
+            // savedTimes
+            // 
+            this.savedTimes.BackColor = System.Drawing.Color.Black;
+            this.savedTimes.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.savedTimes.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.savedTimes.ForeColor = System.Drawing.Color.White;
+            this.savedTimes.FormattingEnabled = true;
+            this.savedTimes.ItemHeight = 19;
+            this.savedTimes.Location = new System.Drawing.Point(0, 0);
+            this.savedTimes.Name = "savedTimes";
+            this.savedTimes.Size = new System.Drawing.Size(98, 152);
+            this.savedTimes.TabIndex = 3;
+            this.savedTimes.TabStop = false;
+            this.savedTimes.KeyDown += new System.Windows.Forms.KeyEventHandler(this.savedTimes_KeyDown);
+            // 
+            // delAllTimesButton
+            // 
+            this.delAllTimesButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.delAllTimesButton.Location = new System.Drawing.Point(104, 120);
+            this.delAllTimesButton.Name = "delAllTimesButton";
+            this.delAllTimesButton.Size = new System.Drawing.Size(60, 25);
+            this.delAllTimesButton.TabIndex = 11;
+            this.delAllTimesButton.TabStop = false;
+            this.delAllTimesButton.Text = "DEL-A";
+            this.delAllTimesButton.UseVisualStyleBackColor = true;
+            this.delAllTimesButton.Click += new System.EventHandler(this.delAllTimesButton_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.averageTimeLabel);
+            this.panel2.Controls.Add(this.lowestTimeLabel);
+            this.panel2.Controls.Add(this.averageTimeText);
+            this.panel2.Controls.Add(this.lowestTimeText);
+            this.panel2.Controls.Add(this.highestTimeText);
+            this.panel2.Controls.Add(this.highestTimeLabel);
+            this.panel2.Location = new System.Drawing.Point(212, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(93, 152);
+            this.panel2.TabIndex = 8;
+            // 
+            // highestTimeLabel
+            // 
+            this.highestTimeLabel.AutoSize = true;
+            this.highestTimeLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.highestTimeLabel.ForeColor = System.Drawing.Color.White;
+            this.highestTimeLabel.Location = new System.Drawing.Point(3, 62);
+            this.highestTimeLabel.Name = "highestTimeLabel";
+            this.highestTimeLabel.Size = new System.Drawing.Size(81, 19);
+            this.highestTimeLabel.TabIndex = 8;
+            this.highestTimeLabel.Text = "Highest:";
+            // 
+            // highestTimeText
+            // 
+            this.highestTimeText.AutoSize = true;
+            this.highestTimeText.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.highestTimeText.ForeColor = System.Drawing.Color.White;
+            this.highestTimeText.Location = new System.Drawing.Point(3, 81);
+            this.highestTimeText.Name = "highestTimeText";
+            this.highestTimeText.Size = new System.Drawing.Size(81, 19);
+            this.highestTimeText.TabIndex = 7;
+            this.highestTimeText.Text = "00:00:00";
+            // 
+            // lowestTimeText
+            // 
+            this.lowestTimeText.AutoSize = true;
+            this.lowestTimeText.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lowestTimeText.ForeColor = System.Drawing.Color.White;
+            this.lowestTimeText.Location = new System.Drawing.Point(3, 119);
+            this.lowestTimeText.Name = "lowestTimeText";
+            this.lowestTimeText.Size = new System.Drawing.Size(81, 19);
+            this.lowestTimeText.TabIndex = 9;
+            this.lowestTimeText.Text = "00:00:00";
+            // 
+            // averageTimeText
+            // 
+            this.averageTimeText.AutoSize = true;
+            this.averageTimeText.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.averageTimeText.ForeColor = System.Drawing.Color.White;
+            this.averageTimeText.Location = new System.Drawing.Point(3, 25);
+            this.averageTimeText.Name = "averageTimeText";
+            this.averageTimeText.Size = new System.Drawing.Size(81, 19);
+            this.averageTimeText.TabIndex = 5;
+            this.averageTimeText.Text = "00:00:00";
+            // 
+            // lowestTimeLabel
+            // 
+            this.lowestTimeLabel.AutoSize = true;
+            this.lowestTimeLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lowestTimeLabel.ForeColor = System.Drawing.Color.White;
+            this.lowestTimeLabel.Location = new System.Drawing.Point(3, 100);
+            this.lowestTimeLabel.Name = "lowestTimeLabel";
+            this.lowestTimeLabel.Size = new System.Drawing.Size(72, 19);
+            this.lowestTimeLabel.TabIndex = 10;
+            this.lowestTimeLabel.Text = "Lowest:";
+            // 
+            // averageTimeLabel
+            // 
+            this.averageTimeLabel.AutoSize = true;
+            this.averageTimeLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.averageTimeLabel.ForeColor = System.Drawing.Color.White;
+            this.averageTimeLabel.Location = new System.Drawing.Point(3, 6);
+            this.averageTimeLabel.Name = "averageTimeLabel";
+            this.averageTimeLabel.Size = new System.Drawing.Size(81, 19);
+            this.averageTimeLabel.TabIndex = 6;
+            this.averageTimeLabel.Text = "Average:";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.delAllTimesButton);
+            this.panel1.Controls.Add(this.savedTimes);
+            this.panel1.Controls.Add(this.delTimeButton);
+            this.panel1.Location = new System.Drawing.Point(25, 110);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(308, 152);
+            this.panel1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(789, 271);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.scrambleText);
+            this.Controls.Add(this.inspectTimerText);
+            this.Controls.Add(this.timerText);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Cube";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label timerText;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label inspectTimerText;
+        private System.Windows.Forms.Timer inspectTimer;
+        private System.Windows.Forms.Label scrambleText;
+        private System.Windows.Forms.Button delTimeButton;
+        private System.Windows.Forms.ListBox savedTimes;
+        private System.Windows.Forms.Button delAllTimesButton;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label averageTimeLabel;
+        private System.Windows.Forms.Label lowestTimeLabel;
+        private System.Windows.Forms.Label averageTimeText;
+        private System.Windows.Forms.Label lowestTimeText;
+        private System.Windows.Forms.Label highestTimeText;
+        private System.Windows.Forms.Label highestTimeLabel;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using cube.Properties;
+
+namespace cube
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            GenerateScramble();
+            InputTimes();
+        }
+
+        #region Variables
+        bool _timerRunning, _timerEnded, _inspectTimerRunning, _keyHeld;
+        int _ms, _averageTime, _lowestTime, _highestTime;
+        //TODO try to use int; convert to float if neccessary
+        float _inspectTime;
+        //TODO try to use Time for inspect; use int
+        #endregion
+
+        #region Key Events
+        void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            _keyHeld = false;
+            if (e.KeyCode == Keys.Space)
+            {
+                if (!_timerEnded)
+                {
+                    switch (_timerRunning)
+                    {
+                        case false:
+                            TimerStart();
+                            break;
+                        case true:
+                            TimerStop();
+                            break;
+                    }
+                }
+                else if (_timerEnded) _timerEnded = false;
+                if (_ms > 0 & !_timerRunning)
+                {
+                    TimerReset();
+                }
+
+            }
+        }
+
+        void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (_keyHeld)
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                _keyHeld = true;
+                if (e.KeyCode == Keys.Space & !_timerRunning)
+                {
+                    timerText.Text = Resources.time_0;
+                    timerText.ForeColor = Color.Lime;
+                    InspectTimerStart();
+                    scrambleText.Visible = false;
+                }
+                else if (_timerRunning)
+                {
+                    TimerStop();
+                }
+            }
+        }
+        #endregion
+
+        #region Timer
+        void timer1_Tick(object sender, EventArgs e)
+        {
+            _ms++;
+            timerText.Text = TimeSpan.FromMilliseconds(_ms * 1000).ToString();
+        }
+
+        void TimerStart()
+        {
+            timerText.ForeColor = Color.White;
+            timer1.Start();
+            _timerRunning = true;
+            InspectTimerStop();
+        }
+
+        void TimerStop()
+        {
+            timer1.Stop();
+            SaveTime();
+            _timerRunning = false;
+            _timerEnded = true;
+            timerText.ForeColor = Color.Red;
+            GenerateScramble();
+        }
+
+        void TimerReset()
+        {
+            _ms = 0;
+        }
+        #endregion
+
+        #region InspectTimer
+        void inspectTimer_Tick(object sender, EventArgs e)
+        {
+            if (_inspectTimerRunning)
+            {
+                if (_inspectTime > 0)
+                {
+                    _inspectTime = _inspectTime - 0.05f;
+                    inspectTimerText.Text = (_inspectTime * 100).ToString("00:00:00");
+                }
+                else
+                {
+                    InspectTimerStop();
+                    timerText.ForeColor = Color.DarkOrange;
+                }
+            }
+        }
+
+        void InspectTimerStart()
+        {
+            InspectTimerReset();
+            inspectTimerText.Visible = true;
+            inspectTimer.Start();
+            _inspectTimerRunning = true;
+        }
+
+        void InspectTimerStop()
+        {
+            inspectTimerText.Visible = false;
+            inspectTimer.Stop();
+            _inspectTimerRunning = false;
+        }
+
+        void InspectTimerReset()
+        {
+            _inspectTime = 15;
+        }
+        #endregion
+
+        #region Times
+        void SaveTime()
+        {
+            savedTimes.Items.Add(timerText.Text);
+            savedTimes.TopIndex = savedTimes.Items.Count - 1;
+            GenerateTimeInfo();
+        }
+
+        void DeleteTime()
+        {
+            savedTimes.Items.Remove(savedTimes.SelectedItem);
+            GenerateTimeInfo();
+        }
+
+        void DeleteAllTimes()
+        {
+            savedTimes.Items.Clear();
+            GenerateTimeInfo();
+        }
+
+        void GenerateTimeInfo()
+        {
+            if (savedTimes.Items.Count > 0)
+            {
+                var timeList = (from string savedTime in savedTimes.Items select TimeSpan.Parse(savedTime).TotalMilliseconds).ToList();
+                _averageTime = Math.Round((timeList.Average()) / 1000) * 1000;
+                averageTimeText.Text = TimeSpan.FromMilliseconds(_averageTime).ToString();
+                _lowestTime = timeList.Min();
+                lowestTimeText.Text = TimeSpan.FromMilliseconds(_lowestTime).ToString();
+                _highestTime = timeList.Max();
+                highestTimeText.Text = TimeSpan.FromMilliseconds(_highestTime).ToString();
+            }
+            else if (savedTimes.Items.Count == 0)
+            {
+                averageTimeText.Text = Resources.time_0;
+                lowestTimeText.Text = Resources.time_0;
+                highestTimeText.Text = Resources.time_0;
+            }
+            OutputTimes();
+            if (savedTimes.Items.Count == 0) DeleteTimesFile();
+        }
+
+        void delTimeButton_Click(object sender, EventArgs e)
+        {
+            DeleteTime();
+        }
+
+        void delAllTimesButton_Click(object sender, EventArgs e)
+        {
+            DeleteAllTimes();
+            DeleteTimesFile();
+        }
+
+        private void savedTimes_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                DeleteTime();
+            }
+        }
+        #endregion
+
+        #region IO
+        void OutputTimes()
+        {
+            StreamWriter stWrite = new StreamWriter("cube-times.txt");
+            foreach (string item in savedTimes.Items)
+            {
+                stWrite.WriteLine(item);
+            }
+            stWrite.Close();
+        }
+
+        void InputTimes()
+        {
+            if (File.Exists("cube-times.txt"))
+            {
+                savedTimes.Items.Clear();
+                using (StreamReader stRead = new StreamReader("cube-times.txt"))
+                {
+                    while (!stRead.EndOfStream)
+                    {
+                        savedTimes.Items.Add(stRead.ReadLine());
+                    }
+                }
+            }
+            GenerateTimeInfo();
+        }
+
+        void DeleteTimesFile()
+        {
+            if (File.Exists("cube-times.txt"))
+            {
+                File.Delete("cube-times.txt");
+            }
+        }
+        #endregion
+
+        #region Scramble
+
+        readonly Random _algGenerator = new Random();
+        readonly string[] _faces = { "U", "D", "L", "R", "B", "F" };
+        readonly string[] _types = { "2", "'", " " };
+        readonly string[] _scramble = new string[25];
+
+        void GenerateScramble()
+        {
+            scrambleText.Visible = true;
+            string[] alg = new string[25];
+            for (int i = 0; i < 25; i++)
+            {
+                string move = _generateMove();
+                while ((i > 0) && (move.Contains(alg[i - 1])))
+                {
+                    move = _generateMove();
+                }
+                alg[i] = move.Substring(0, 1);
+                _scramble[i] = (move);
+            }
+            scrambleText.Text = "";
+            foreach (string s in _scramble)
+            {
+                scrambleText.Text = String.Format("{0}{1} ", scrambleText.Text, s);
+            }
+        }
+
+        string _generateMove()
+        {
+            string face = _faces[_algGenerator.Next(_faces.Length)];
+            string type = _types[_algGenerator.Next(_types.Length)];
+            string move = face + type;
+            return move;
+        }
+
+        #endregion
+    }
+}
